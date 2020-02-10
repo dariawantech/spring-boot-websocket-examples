b775a749a53ed768f50d6b6115ceb2e901a8b2e6
teodesson <teodesson@gmail.com>
2020-02-10 22:12:06 +0800

    Initial commit - Spring Boot + Plain WebSocket application

---
+++ src/main/java/com/dariawan/websocket/handler/MyTextWebSocketHandler.java
@@ -0,0 +1,80 @@
+/**
+ * Spring Boot + WebSocket Examples  (https://www.dariawan.com)
+ * Copyright (C) 2020 Dariawan <hello@dariawan.com>
+ *
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ * Under this license, you are free to:
+ * # Share - copy and redistribute the material in any medium or format
+ * # Adapt - remix, transform, and build upon the material for any purpose,
+ *   even commercially.
+ *
+ * The licensor cannot revoke these freedoms
+ * as long as you follow the license terms.
+ *
+ * License terms:
+ * # Attribution - You must give appropriate credit, provide a link to the
+ *   license, and indicate if changes were made. You may do so in any
+ *   reasonable manner, but not in any way that suggests the licensor
+ *   endorses you or your use.
+ * # ShareAlike - If you remix, transform, or build upon the material, you must
+ *   distribute your contributions under the same license as the original.
+ * # No additional restrictions - You may not apply legal terms or
+ *   technological measures that legally restrict others from doing anything the
+ *   license permits.
+ *
+ * Notices:
+ * # You do not have to comply with the license for elements of the material in
+ *   the public domain or where your use is permitted by an applicable exception
+ *   or limitation.
+ * # No warranties are given. The license may not give you all of
+ *   the permissions necessary for your intended use. For example, other rights
+ *   such as publicity, privacy, or moral rights may limit how you use
+ *   the material.
+ *
+ * You may obtain a copy of the License at
+ *   https://creativecommons.org/licenses/by-sa/4.0/
+ *   https://creativecommons.org/licenses/by-sa/4.0/legalcode
+ */
+package com.dariawan.websocket.handler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyTextWebSocketHandler extends TextWebSocketHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyTextWebSocketHandler.class);
+    
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        sessions.forEach(webSocketSession -> {
+            try {
+                webSocketSession.sendMessage(message);
+            } catch (IOException e) {
+                LOGGER.error("Error occurred.", e);
+            }
+        });
+    }
+}
